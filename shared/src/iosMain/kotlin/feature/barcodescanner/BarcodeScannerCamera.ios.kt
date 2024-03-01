27f8d8ef37da64a1b5ba86f8c516a429448e0bdc
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2024-03-01 11:11:20 +0300

    Add implementation barcode scanner for iOS (not working yet)

--- shared/src/iosMain/kotlin/feature/barcodescanner/BarcodeScannerCamera.ios.kt
+++ shared/src/iosMain/kotlin/feature/barcodescanner/BarcodeScannerCamera.ios.kt
@@ -2,6 +2,8 @@ package feature.barcodescanner
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,44 +12,90 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.UIKitView
-import platform.UIKit.UIImage
-import platform.UIKit.UIImagePickerController
-import platform.UIKit.UIImagePickerControllerDelegateProtocol
-import platform.UIKit.UIImagePickerControllerSourceType
-import platform.UIKit.UINavigationControllerDelegateProtocol
-import platform.darwin.NSObject
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import platform.AVFoundation.AVCaptureDevice
+import platform.AVFoundation.AVCaptureDeviceInput
+import platform.AVFoundation.AVCaptureMetadataOutput
+import platform.AVFoundation.AVCaptureSession
+import platform.AVFoundation.AVCaptureVideoPreviewLayer
+import platform.AVFoundation.AVMediaTypeVideo
+import platform.CoreGraphics.CGRectMake
+import platform.UIKit.UIColor
+import platform.UIKit.UIView
 
 @Composable
 actual fun BarcodeScannerCamera() {
     var code by remember { mutableStateOf(value = "") }
-    val imagePickerController = remember { UIImagePickerController() }
-    imagePickerController.sourceType =
-        UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera
-    imagePickerController.delegate = object : NSObject(), UIImagePickerControllerDelegateProtocol,
-        UINavigationControllerDelegateProtocol {
-        override fun imagePickerController(
-            picker: UIImagePickerController,
-            didFinishPickingImage: UIImage,
-            editingInfo: Map<Any?, *>?
-        ) {
-            val analyzer = BarcodeScannerAnalyzer {scannedCode ->
-                code = scannedCode
-            }
-            analyzer.analyze(didFinishPickingImage)
-        }
-    }
 
+    // 1. Настроим сессию
+    val session = remember { AVCaptureSession() }
+    // 2. Настраиваем устройство видео
+    val captureDevice =
+        remember { AVCaptureDevice.defaultDeviceWithMediaType(mediaType = AVMediaTypeVideo) }
+    // 3. Настроим input
+//    try {
+    val input = remember { AVCaptureDeviceInput(device = captureDevice!!, error = null) }
+    session.addInput(input = input)
+//    } catch (e: Exception) {
+
+//    }
+    // 4. Настроим output
+    val output = remember { AVCaptureMetadataOutput() }
+    session.addOutput(output)
+
+//    output.setMetadataObjectsDelegate(
+//        objectsDelegate = object : NSObject(), AVCaptureMetadataOutputObjectsDelegateProtocol {
+//            override fun captureOutput(
+//                output: AVCaptureOutput,
+//                didOutputMetadataObjects: List<*>,
+//                fromConnection: AVCaptureConnection
+//            ) {
+//
+//            }
+//        },
+//        queue = dispatch_queue_main_t()
+//    )
+//    output.metadataObjectTypes = listOf(AVMetadataObjectTypeQRCode)
+    output.metadataObjectTypes = output.availableMetadataObjectTypes
+
+    val video = remember { AVCaptureVideoPreviewLayer(sessionWithNoConnection = session) }
+
+    val view = remember { UIView() }
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
         UIKitView(
             factory = {
-                imagePickerController.view
+                view.backgroundColor = UIColor.blueColor
+
+                video.backgroundColor = UIColor.redColor().CGColor
+                video.frame = CGRectMake(x = 0.0, y = 100.0, width = 200.0, height = 200.0)
+//                video.frame = view.layer.bounds
+                view
             },
+//            onResize = { view: UIView, rect: CValue<CGRect> ->
+//                video.frame = view.layer.bounds
+//            },
+//            update = { view: UIView ->
+//                session.startRunning()
+//            },
             modifier = Modifier
-                .fillMaxSize()
+                .size(400.dp),
         )
         Text(text = "Code: $code")
+        Button(
+            onClick = {
+                view.layer.addSublayer(video)
+                CoroutineScope(Dispatchers.Default).launch {
+                    session.startRunning()
+                }
+            },
+        ) {
+            Text("Click my scan")
+        }
     }
 }
